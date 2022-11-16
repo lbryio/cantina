d94dcba9c9d5cc761d47a897ffd6e230ef53b57c
Daniel Krol <orblivion@gmail.com>
2022-11-16 09:47:04 -0500

    SDK interface finally sort of works

--- src/sdk/sdk.go
+++ src/sdk/sdk.go
@@ -3,6 +3,7 @@ package sdk
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"net/http"
 )
@@ -16,23 +17,32 @@ const SDK_URL = "http://localhost:5279"
 type ClaimId string
 type ClaimName string
 
-type ClaimResponse struct {
-	Result struct {
-		Items []Claim `json:"items"`
-		// TODO "blocked" - ?
-	} `json:"result"`
-}
-
 type Claim struct {
 	ClaimId      ClaimId   `json:"claim_id"`
 	PermanentURL string    `json:"permanent_url"`
 	Name         ClaimName `json:"name"`
 }
 
+type ClaimResponse struct {
+	Result struct {
+		Items []Claim `json:"items"`
+		// TODO? Blocked map[string]interface{} `json:"blocked"`
+	} `json:"result"`
+}
+
+type ClaimNotFound struct {
+}
+
+func (ce *ClaimNotFound) Error() string {
+	return "Claim not found"
+}
+
 func GetClaim(claimId ClaimId) (claim Claim, err error) {
-	reqBodyStr, _ := json.Marshal(map[string]string{
-		"method":   "claim_search",
-		"claim_id": string(claimId),
+	reqBodyStr, _ := json.Marshal(map[string]interface{}{
+		"method": "claim_search",
+		"params": map[string]string{
+			"claim_id": string(claimId),
+		},
 	})
 
 	resp, err := http.Post(SDK_URL, "application/json", bytes.NewBuffer(reqBodyStr))
@@ -41,11 +51,21 @@ func GetClaim(claimId ClaimId) (claim Claim, err error) {
 	}
 	defer resp.Body.Close()
 
-	var respBody ClaimResponse
 	respBodyStr, _ := ioutil.ReadAll(resp.Body)
-	err = json.Unmarshal(respBodyStr, respBody)
 
-	claim = respBody.Result.Items[0]
+	var respBody ClaimResponse
+	err = json.Unmarshal(respBodyStr, &respBody)
+	if err != nil {
+		return
+	}
+
+	if len(respBody.Result.Items) == 0 {
+		err = &ClaimNotFound{}
+	} else if len(respBody.Result.Items) > 1 {
+		err = fmt.Errorf("Multiple found for some reason. Skipping.")
+	} else {
+		claim = respBody.Result.Items[0]
+	}
 
 	return
 }

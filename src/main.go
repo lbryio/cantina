b07fb3ce1128b8757bdb1881991879eae7530f90
Daniel Krol <orblivion@gmail.com>
2022-11-16 09:34:58 -0500

    Error responses

--- src/main.go
+++ src/main.go
@@ -1,9 +1,49 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"lbryio/cantina/objects"
 	"lbryio/cantina/sdk"
+	"log"
 	"net/http"
+	"strings"
 )
+
+// TODO - Does ActivityPub specify a specific error format?
+type ErrorResponse struct {
+	Error string `json:"error"`
+}
+
+func readableErrorJson(w http.ResponseWriter, code int, extra string) {
+	errorStr := http.StatusText(code)
+	if extra != "" {
+		errorStr = errorStr + ": " + extra
+	}
+	errorJson, err := json.Marshal(ErrorResponse{Error: errorStr})
+	if err != nil {
+		// In case something really stupid happens
+		http.Error(w, `{"error": "error when JSON-encoding error message"}`, code)
+	}
+	http.Error(w, string(errorJson), code)
+	return
+}
+
+// Don't report any details to the user. Log it instead.
+func internalServiceErrorJson(w http.ResponseWriter, serverErr error, errContext string) {
+	errorStr := http.StatusText(http.StatusInternalServerError)
+	errorJson, err := json.Marshal(ErrorResponse{Error: errorStr})
+	if err != nil {
+		// In case something really stupid happens
+		http.Error(w, `{"error": "error when JSON-encoding error message"}`, http.StatusInternalServerError)
+		log.Printf("error when JSON-encoding error message")
+		return
+	}
+	http.Error(w, string(errorJson), http.StatusInternalServerError)
+	log.Printf("%s: %+v\n", errContext, serverErr)
+
+	return
+}
 
 func handleChannel(w http.ResponseWriter, req *http.Request) {
 	if req.Method == http.MethodGet {
